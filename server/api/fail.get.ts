6a80e628e2dc24ba5a6fbc4d3a0060010812d11c
revortni <rojit.blaze13@gmail.com>
2025-09-01 14:07:46 +0545

    Add api handler with max duration 1s and runtime 3s

---
+++ server/api/fail.get.ts
@@ -0,0 +1,12 @@
+export const runtime = 'nodejs';
+export const maxDuration = 1;
+
+export default defineEventHandler(async () => {
+  const res = await new Promise<{ message: string }>((res) => {
+    setTimeout(() => {
+      res({ message: 'Successfully resolved in 3 seconds' })
+    }, 3000)
+  })
+
+  return Response.json(res);
+})
