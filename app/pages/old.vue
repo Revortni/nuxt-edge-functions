abc95a8134d6b571bccf11368c66a334e509bc10
revortni <rojit.blaze13@gmail.com>
2025-09-05 13:17:42 +0545

    Add sample route rules

---
+++ app/pages/old.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>OLD page</div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,20 @@ export default defineNuxtConfig({
     exposeConfig: true,
     viewer: true,
     // and more...
+  },
+  routeRules: {
+    '/': { prerender: true },
+    // user dashboard renders only on client-side
+    '/me/**': { ssr: false },
+    // Add cors headers on API routes
+    '/api/**': { cors: true },
+    // Redirects legacy urls
+    '/old': { redirect: '/new' }
   }
+  // use if pure client side rendering
+  // hooks: {
+  //   'prerender:routes'({ routes }) {
+  //     routes.clear() // Do not generate any routes (except the defaults)
+  //   }
+  // },
 })
