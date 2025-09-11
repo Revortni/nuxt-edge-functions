ccfcc6976c32544103a2b5b0e571b51189f883c3
revortni <rojit.blaze13@gmail.com>
2025-09-11 10:52:28 +0545

    Remove client only from login button

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="w-full flex justify-center text-purple-700 flex-col p-16 gap-4">
     <h1>Login</h1>
-    <client-only>
-      <button
-        class="p-4 text-white bg-purple-900"
-        @click="handleLogin"
-      >
-        Login with Auth0
-      </button>
-    </client-only>
+    <button
+      class="p-4 text-white bg-purple-900"
+      @click="handleLogin"
+    >
+      Login with Auth0
+    </button>
   </div>
 </template>
 
